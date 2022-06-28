b3e8ec103c00716aa39356566b4e5a2dbb74d7d3
Kenneth Parrales <kenneth.parrales@gmail.com>
2022-06-28 12:08:44 -0600

    Correccion de Archivos javascript

--- Tienda_ropa/Capa_presentacion/wwwroot/js/site.js
+++ Tienda_ropa/Capa_presentacion/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function modalFunction(DivModal) {
+    if (DivModal.style.opacity == 0) {
+        DivModal.style.transition = "all ease 1s";
+        DivModal.style.opacity = 1;
+        DivModal.style.pointerEvents = "all";
+    } else {
+        DivModal.style.transition = "all ease 1s";
+        DivModal.style.opacity = 0;
+        DivModal.style.pointerEvents = "none";
+    }
+}
